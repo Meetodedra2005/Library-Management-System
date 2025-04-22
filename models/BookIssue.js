2b58b271905440a4d488e3945fb2c804bc688ca6
DevenNirmal-109 <devennirmal.ce@gmail.com>
2025-04-22 21:25:36 +0530

    This is final changes

--- models/BookIssue.js
+++ models/BookIssue.js
@@ -5,7 +5,11 @@ const BookIssueSchema = new mongoose.Schema({
     Student_id: { type: String, required: true },
     Sem: { type: Number, required: true },
     Book_name: { type: String, required: true },
-    Issue_date: { type: Date, default: Date.now }
+    Issue_date: { type: Date, default: Date.now },
+    Return_date: { 
+        type: Date, 
+        default: () => new Date(Date.now() + 60 * 1000) // current time + 1 minute
+    }
 });
 
 module.exports = mongoose.models.BookIssue || mongoose.model('BookIssue', BookIssueSchema);
